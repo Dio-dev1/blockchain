30cecc1e6650ae2894b99c7d013415c2d76ebc6d
Dio-dev1 <VictorySupper@gmail.com>
2021-03-28 00:22:40 +0800

    backend url change with live url

--- src/rest/http-common.js
+++ src/rest/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "http://node-env.eba-kvks79ht.us-east-2.elasticbeanstalk.com/",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
