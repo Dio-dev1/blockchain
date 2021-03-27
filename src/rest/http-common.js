d18fdb0f55b4cfc3e28e7ffb0facfd2ff2a3b372
Dio-dev1 <VictorySupper@gmail.com>
2021-03-28 00:33:25 +0800

    change base url

--- src/rest/http-common.js
+++ src/rest/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://node-env.eba-kvks79ht.us-east-2.elasticbeanstalk.com/",
+  baseURL: "http://node-env.eba-kvks79ht.us-east-2.elasticbeanstalk.com/api",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
